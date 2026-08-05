6f62accb47a6053250cad1386e21214f0156bf94
Lindascrub <165819955+Lindascrub@users.noreply.github.com>
2026-08-05 12:50:15 +0200

    categoria aggiunta descrizione

--- src/main/java/it/uniroma3/siw/model/Categoria.java
+++ src/main/java/it/uniroma3/siw/model/Categoria.java
@@ -1,5 +1,8 @@
 package it.uniroma3.siw.model;
 
+import java.util.List;
+import java.util.Objects;
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
@@ -10,37 +13,77 @@ public class Categoria {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     
-    @Column(nullable = false)
+    @Column(unique = true)
     @NotBlank(message = "Il nome è obbligatorio")
     private String nome;
-  
     
-    // Costruttore vuoto (obbligatorio per JPA)
-    public Categoria() {}
+    @NotBlank(message = "La descrizione è obbligatoria")
+    @Column(length = 2000)
+	private String descrizione;
+  
+    @OneToMany(mappedBy = "categoria")
+    private List<Prodotto> prodotti;
+    
+    // Costruttore vuoto 
+    public Categoria() {
+    	
+    }
     
     // Costruttore con parametri
     public Categoria(String nome, String descrizione) {
         this.nome = nome;
+        this.descrizione = descrizione;
     }
     
     // GETTER e SETTER
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
-    
-    // equals e hashCode (utili per confrontare oggetti)
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Categoria categoria = (Categoria) o;
-        return id != null && id.equals(categoria.id);
-    }
-    
-    @Override
-    public int hashCode() {
-        return id != null ? id.hashCode() : 0;
-    }
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public List<Prodotto> getProdotti() {
+		return prodotti;
+	}
+
+	public void setProdotti(List<Prodotto> prodotti) {
+		this.prodotti = prodotti;
+	}
+    // equals e hashCode 
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+   
 }
